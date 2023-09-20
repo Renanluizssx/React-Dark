10c376affb37c0c2472d5ccaa111f3c48a03c2b1
Renanluizssx <renanluizssx@gmail.com>
2023-09-20 13:48:59 -0300

    first commit

---
+++ src/Componentes/Topo/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./topo.css";
+import Logo from "../../Assets/logo.png"
+function Topo({mode, setmode, changeIcons, changeBackgroundButton
+}) {
+  const verificarEstado = () => (
+    mode === "modo-claro" ? setmode("modo-escuro") : setmode("modo-claro")  )
+  return (
+    <div className={mode}>
+      <header>
+        <div>
+          <img className="logo" src={Logo} alt="Logo" />
+        </div>
+        <button  className={changeBackgroundButton}
+          onClick={verificarEstado}
+        >
+          <div>
+            <img
+            src={changeIcons}
+              className="botao"
+              alt="moon"
+            />
+          </div>
+        </button>
+      </header>
+    </div>
+  );
+}
+export default Topo;
