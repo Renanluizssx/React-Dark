6673ebe5cae770815b8e11b1b260c175ca0c6369
Renanluizssx <renanluizssx@gmail.com>
2023-11-15 18:20:22 -0300

    commitado

--- myapp/src/Componentes/Topo/index.js
+++ myapp/src/Componentes/Topo/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import "./topo.css";
-import Logo from "../../Assets/logo.png"
-function Topo({mode, setmode, changeIcons, changeBackgroundButton
-}) {
-  const verificarEstado = () => (
-    mode === "modo-claro" ? setmode("modo-escuro") : setmode("modo-claro")  )
+import Logo from "../../Assets/logo.png";
+function Topo({ mode, setmode, changeIcons, changeBackgroundButton }) {
+  const verificarEstado = () =>
+    mode === "modo-claro" ? setmode("modo-escuro") : setmode("modo-claro");
   return (
     <div className={mode}>
       <header>
         <div>
           <img className="logo" src={Logo} alt="Logo" />
         </div>
-        <button  className={changeBackgroundButton}
-          onClick={verificarEstado}
-        >
+        <button className={changeBackgroundButton} onClick={verificarEstado}>
           <div>
-            <img
-            src={changeIcons}
-              className="botao"
-              alt="moon"
-            />
+            <img src={changeIcons} className="botao" alt="moon" />
           </div>
         </button>
       </header>
